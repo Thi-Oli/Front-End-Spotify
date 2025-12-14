a74cb0c5196753ad015bdc9bbf7d27086d0559b4
Thiago Oliveira <geovanithiago08@gmail.com>
2025-12-14 19:51:30 -0300

    Mudança da URL Front-end

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:3001";
+const URL = "https://back-end-spotify.vercel.app/";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
